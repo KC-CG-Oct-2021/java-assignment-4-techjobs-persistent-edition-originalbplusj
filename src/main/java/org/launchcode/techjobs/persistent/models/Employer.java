578d533c4c99521693936d06265d8d69855c0b18
Beth Johnson <originalbplusj@yahoo.com>
2022-06-27 17:17:23 -0500

    Part 2 complete - SQL test fail

--- src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
@@ -1,6 +1,23 @@
 package org.launchcode.techjobs.persistent.models;
 
+import javax.persistence.Entity;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Entity
 public class Employer extends AbstractEntity {
 
+    @NotBlank(message = "Location cannot be blank")
+    @Size(max = 50, message = "Must not exceed 50 characters")
+    private String location;
 
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+   public Employer(){}
 }
